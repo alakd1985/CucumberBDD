178d8459552f042670390d556fc34bd3bdd12f55
Alak Kanti Dutta”git config --global user.email alakd1985@gmail.com” <alakd1985@gmail.com>
2021-01-17 18:15:56 -0500

    added the username and pass in the feature file

--- Feature/LogIn_Test.feature
+++ Feature/LogIn_Test.feature
@@ -3,7 +3,7 @@ Feature: Login Action
 Scenario: Successful Login with Valid Credentials
  Given User is on Home Page
  When User Navigate to LogIn Page
- And User enters UserName and Password
+ And User enters "admin" and "password"
  Then Message displayed Login Successfully
  
 Scenario: Successful LogOut
--- src/main/java/stepDefinition/Test_Steps.java
+++ src/main/java/stepDefinition/Test_Steps.java
@@ -15,7 +15,7 @@ public class Test_Steps {
 	private static WebDriver driver = null;
 
 	@Given("^User is on Home Page$")
-	public void user_is_on_Home_Page() throws Throwable {
+	public void user_is_on_Home_Page() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -24,32 +24,32 @@ public class Test_Steps {
 	}
 
 	@When("^User Navigate to LogIn Page$")
-	public void user_Navigate_to_LogIn_Page() throws Throwable {
+	public void user_Navigate_to_LogIn_Page() {
 		driver.get("http://localhost:8888/index.php?action=Login&module=Users");
 
 	}
 
-	@When("^User enters UserName and Password$")
-	public void user_enters_UserName_and_Password() throws Throwable {
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("admin");
-		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("password");
+	@When("^User enters \"(.*)\" and \"(.*)\"$")
+	public void user_enters_UserName_and_Password(String username, String pass) {
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys(username);
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys(pass);
 		driver.findElement(By.xpath("//input[@id='submitButton']")).click();
 	}
 
 	@Then("^Message displayed Login Successfully$")
-	public void message_displayed_Login_Successfully() throws Throwable {
+	public void message_displayed_Login_Successfully() {
 		System.out.println("Login Successfully");
 	}
 
 	@When("^User LogOut from the Application$")
-	public void user_LogOut_from_the_Application() throws Throwable {
+	public void user_LogOut_from_the_Application() {
 
 		driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]")).click();
 	}
 
 	@Then("^Message displayed LogOut Successfully$")
-	public void message_displayed_LogOut_Successfully() throws Throwable {
+	public void message_displayed_LogOut_Successfully() {
 		driver.close();
 		System.out.println("LogOut Successfully");
 	}
